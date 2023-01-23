e7f865b3e012a29b029da4a038d61d7e99c4f629
Georgiiio <gosha.pronkin@mail.ru>
2023-01-23 19:13:53 +0100

    added drawing a square

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+function draw() {
+  var canvas = document.getElementById("square");
+  var ctx = canvas.getContext("2d");
+  ctx.rect(10, 10, 100, 100);
+  ctx.fill();
+}
+
 function onAppReady() {
   console.log('document ready')
+  draw()
 }
 
 document.addEventListener('DOMContentLoaded', onAppReady);
