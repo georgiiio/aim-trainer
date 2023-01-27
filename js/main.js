3e6d9a20575737251b366dd429d3ce6d072faeb3
Georgiiio <gosha.pronkin@mail.ru>
2023-01-27 16:28:26 +0100

    render first target

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 16;
+
+let target = {
+  x: 160,
+  y: 160,
+}
+
 function draw() {
-  var canvas = document.getElementById("square");
-  var ctx = canvas.getContext("2d");
-  ctx.rect(10, 10, 100, 100);
-  ctx.fill();
+  context.fillStyle = 'white';
+  context.fillRect(target.x, target.y, grid, grid);
 }
 
 function onAppReady() {
-  console.log('document ready')
   draw()
 }
 
